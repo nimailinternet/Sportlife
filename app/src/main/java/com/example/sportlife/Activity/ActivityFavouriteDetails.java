336a8155d1b58309d98392bad66449983f900157
FIXPEL <fixpel543@mail.ru>
2026-05-18 22:15:17 +0300

    Dialog - profile 2.0

---
+++ app/src/main/java/com/example/sportlife/Activity/ActivityFavouriteDetails.java
@@ -0,0 +1,13 @@
+package com.example.sportlife.Activity;
+
+public class ActivityFavouriteDetails extends CreateActivity {
+    @Override
+    protected int getIdLayout() {
+        return 0;
+    }
+
+    @Override
+    protected int getIdView() {
+        return 0;
+    }
+}
